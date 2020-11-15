ff7078f9542981d20ffdcea6539f4b8df87d99fb
Yutaro Ishiwata <yutaro.ishiwata@gmail.com>
2020-11-15 18:16:08 +0900

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+"use strict";
+
+// Load plugins
+const gulp = require('gulp');
+const path = require('path');
+const rename = require("gulp-rename");
+const unzip = require('gulp-unzip');
+const glob = require("glob");
+
+// Zip file location
+const zipFiles = glob.sync('./sketch/**/*.zip');
+
+// Tasks
+function sketchRename() {
+  return gulp
+    .src('./sketch/**/*.sketch', { base: '.' })
+    .pipe(rename(function (path) {
+      path.extname = ".zip";
+    }))
+    .pipe(gulp.dest('.')) // Current working directory
+}
+
+function sketchUnzip(done) {
+  zipFiles.forEach(function (zipFile) {
+    const zipFileDir = path.dirname(zipFile);
+      
+    return gulp
+      .src(zipFile)
+      .pipe(unzip({ keepEmpty: true }))
+      .pipe(gulp.dest(zipFileDir)) // Same as zip file location
+  })
+  done();
+}
+
+// define complex tasks
+const build = gulp.series(sketchRename, sketchUnzip);
+
+// export tasks
+exports.default = build;
